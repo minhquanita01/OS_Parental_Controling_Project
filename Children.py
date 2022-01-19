2602f55b8ee1a6125010ffb4f26b80f77b8a6754
taii217 <taiinguyen217@gmail.com>
2022-01-19 19:55:54 +0700

    a

--- Children.py
+++ Children.py
@@ -11,12 +11,21 @@ def data_split(data:str):
     sum_ = 0
     d = data.split()
     for s in d:
-        if(s[0] == 'F'): from_ = s[1:]
-        elif(s[0] == 'T'): to_ = s[1:]
+        if(s[0] == 'F'): from_ = s[1:] # string "06:00"
+        elif(s[0] == 'T'): to_ = s[1:] # string "06:00"
         elif(s[0] == 'D'): duration_ = int(s[1:])
         elif(s[0] == 'I'): interrupt_ = int(s[1:])
         elif(s[0] == 'S'): sum_ = int(s[1:])
     return from_, to_, duration_,interrupt_, sum_
+
+def read_file(file_name):
+    time_list = []
+    with open(file_name, encoding = 'utf-8') as f:
+        for s_line in f:
+            f,t,d,i,s = data_split(s_line)           
+            ti = us.Using_Time(f,t,d,i,s)
+            time_list.append(ti)
+    return time_list
 
 def main():
     # parent_pass = "taolabomay"
@@ -35,15 +44,4 @@ def main():
     #         print("Dung mat khau con tre")
     #     else:
             # print("Sai mat khau")
-    
-    time_process:us.Using_Time = []
-    with open("time.txt", encoding = 'utf-8') as f:
-        for s_line in f:
-            f,t,d,i,s = data_split(s_line)           
-            ti = us.Using_Time(f,t,d,i,s)
-            time_process.append(ti)
-
-    if time_process[0].isTimeUsing() : print("oke")
-    
-
-main()
+    pass
--- rsa.py
+++ rsa.py
@@ -1,5 +1,7 @@
 import math
 import random
+
+# mật mã mã hoá mật khẩu rsa
 class rsa:
     def __init__(self):
         self.p = 0
@@ -9,6 +11,8 @@ class rsa:
         self.e = 0
         self.d = 0
         self.m = ""
+
+    #encryto with no key public
     def encryto(self,pri_pass):
         self.p = genPrime(1000,10000)
         self.q = genPrime(1000,10000)
@@ -21,6 +25,7 @@ class rsa:
         self.m = self.encryto_withE(pri_pass,self.e,self.n)
         return self.m,self.e,self.d
     
+    #encryto with key public
     def encryto_withE(self,private_pass,e,n):
         m =""
         for i in private_pass:
@@ -28,11 +33,12 @@ class rsa:
             m += str(t); 
         return m
 
-    #11.01.2001
+    #check_pass with psw
     def isTrue(self,psw):
         psw_en = self.encryto_withE(psw,self.e,self.n)
         return psw_en == self.m
-    
+
+#find modulo e
 def Arbitrary_Int_e(phi):
     e = random.randint(1,phi)
     if(int(gcd(e, phi)) == 1):
@@ -85,5 +91,6 @@ def main():
     print(psw_en)
     print(e)
     print(d)
+    pass
 
 
